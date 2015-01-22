03aac4d2c69ff0172ea3b12ff129539b5fec24fa
Anselm Brehme <Faithcaio@cubeisland.de>
2015-01-22 16:54:34 +0100

    CommandContext -> CommandSender

--- chat/src/main/java/de/cubeisland/engine/module/chat/ChatCommands.java
+++ chat/src/main/java/de/cubeisland/engine/module/chat/ChatCommands.java
@@ -22,6 +22,7 @@ import de.cubeisland.engine.command.parametric.Greed;
 import de.cubeisland.engine.command.parametric.Label;
 import de.cubeisland.engine.command.parametric.Optional;
 import de.cubeisland.engine.core.command.CommandContext;
+import de.cubeisland.engine.core.command.CommandSender;
 import de.cubeisland.engine.core.user.User;
 
 import static de.cubeisland.engine.command.parameter.Parameter.INFINITE;
@@ -38,42 +39,42 @@ public class ChatCommands
     }
 
     @Command(desc = "Allows you to emote")
-    public void me(CommandContext context, @Greed(INFINITE) String message)
+    public void me(CommandSender context, @Greed(INFINITE) String message)
     {
-        this.module.getCore().getUserManager().broadcastStatus(message, context.getSource());
+        this.module.getCore().getUserManager().broadcastStatus(message, context);
     }
 
     @Command(desc = "Changes your display name")
-    public void nick(CommandContext context, @Label("<name>|-reset")String name, @Optional User player)
+    public void nick(CommandSender context, @Label("<name>|-reset") String name, @Optional User player)
     {
         if (player == null)
         {
-            if (!(context.getSource() instanceof User))
+            if (!(context instanceof User))
             {
                 context.sendTranslated(NEGATIVE, "You cannot change the consoles display name"); // TODO You cannot?!?
                 return;
             }
-            player = (User)context.getSource();
+            player = (User)context;
         }
 
-        if (!context.getSource().equals(player) && !module.perms().COMMAND_NICK_OTHER.isAuthorized(context.getSource()))
+        if (!context.equals(player) && !module.perms().COMMAND_NICK_OTHER.isAuthorized(context))
         {
             context.sendTranslated(NEGATIVE, "You are not allowed to change the nickname of another player!");
             return;
         }
 
-        if (name.equalsIgnoreCase("-r") || name.equalsIgnoreCase("-reset"))
+        if ("-r".equalsIgnoreCase(name) || "-reset".equalsIgnoreCase(name))
         {
-            player.setDisplayName(context.getSource().getName());
-            context.sendTranslated(POSITIVE, "Display name reset to {user}", context.getSource());
+            player.setDisplayName(context.getName());
+            context.sendTranslated(POSITIVE, "Display name reset to {user}", context);
             return;
         }
-        if (module.getCore().getUserManager().findExactUser(name) != null && !module.perms().COMMAND_NICK_OFOTHER.isAuthorized(context.getSource()))
+        if (module.getCore().getUserManager().findExactUser(name) != null && !module.perms().COMMAND_NICK_OFOTHER.isAuthorized(context))
         {
             context.sendTranslated(NEGATIVE, "This name has been taken by another player!");
             return;
         }
-        context.sendTranslated(POSITIVE, "Display name changed from {user} to {user}", context.getSource(), name);
-        ((User)context.getSource()).setDisplayName(name);
+        context.sendTranslated(POSITIVE, "Display name changed from {user} to {user}", context, name);
+        ((User)context).setDisplayName(name);
     }
 }
