e294ad5db0cba54fdb55be4e5fcda05e8a4911fd
Anselm Brehme <Faithcaio@cubeisland.de>
2015-02-28 12:36:53 +0100

    refactoring Writer

--- writer/src/main/java/de/cubeisland/engine/module/writer/Writer.java
+++ writer/src/main/java/de/cubeisland/engine/module/writer/Writer.java
@@ -17,11 +17,12 @@
  */
 package de.cubeisland.engine.module.writer;
 
-import java.util.Collections;
+import java.util.Set;
 import de.cubeisland.engine.command.filter.Restricted;
 import de.cubeisland.engine.command.parametric.Command;
 import de.cubeisland.engine.command.parametric.Label;
 import de.cubeisland.engine.command.parametric.Named;
+import de.cubeisland.engine.core.command.CommandManager;
 import de.cubeisland.engine.core.module.Module;
 import de.cubeisland.engine.core.user.User;
 import org.bukkit.Material;
@@ -33,104 +34,104 @@ import org.bukkit.inventory.meta.BookMeta;
 
 import static de.cubeisland.engine.core.util.formatter.MessageType.NEGATIVE;
 import static de.cubeisland.engine.core.util.formatter.MessageType.POSITIVE;
-import static org.bukkit.Material.SIGN_POST;
-import static org.bukkit.Material.WALL_SIGN;
+import static org.bukkit.Material.*;
 
 /**
- * A module to edit signs and/or unsign written books
+ * A module to edit signs and signed books
  */
 public class Writer extends Module
 {
     @Override
     public void onEnable()
     {
-        this.getCore().getCommandManager().addCommands(this.getCore().getCommandManager(), this, this);
+        CommandManager cm = this.getCore().getCommandManager();
+        cm.addCommands(cm, this, this);
     }
 
-    @Command(alias = "rewrite", desc = "Edit a sign or unsign a book")
+    @Command(alias = "rewrite", desc = "Edit a sign or a signed book")
     @Restricted(value = User.class, msg = "Edit what?")
     public void edit(User context,
                      @Named({"1", "Line1"}) @Label("1st line") String line1,
                      @Named({"2", "Line2"}) @Label("2st line") String line2,
                      @Named({"3", "Line3"}) @Label("3st line") String line3,
                      @Named({"4", "Line4"}) @Label("4st line") String line4)
-
     {
-        if (!this.unsignBook(context))
+        if (line1 == null && line2 == null && line3 == null && line4 == null)
         {
-            if (line1 == null && line2 == null && line3 == null && line4 == null)
+            if (!this.editBookInHand(context))
             {
                 context.sendTranslated(NEGATIVE, "You need to specify at least one parameter to edit a sign!");
                 context.sendTranslated(NEGATIVE, "Or hold a signed book in your hand to edit it.");
-                return;
             }
-            if (!this.editSignInSight(context, line1, line2, line3, line4))
-            {
-                context.sendTranslated(NEGATIVE,
-                                    "You need to have a signed book in hand or be looking at a sign less than 10 blocks away!");
-            }
+            return;
         }
+        if (!this.editSignInSight(context, line1, line2, line3, line4))
+        {
+            context.sendTranslated(NEGATIVE, "You need to be looking at a sign less than {amount} blocks away!", 10);
+            context.sendTranslated(NEGATIVE, "Or hold a signed book in your hand to edit it.");
+        }
+    }
+
+    /**
+     * Makes a signed book in the hand of given user editable
+     *
+     * @param user the user
+     *
+     * @return false if there is no written book in the hand of given user
+     */
+    public boolean editBookInHand(User user)
+    {
+        if (user.getItemInHand().getType() != WRITTEN_BOOK)
+        {
+            return false;
+        }
+        ItemStack item = user.getItemInHand();
+        BookMeta meta = ((BookMeta)item.getItemMeta());
+        meta.setAuthor("");
+        meta.setTitle("");
+        item.setItemMeta(meta);
+        item.setType(BOOK_AND_QUILL);
+        user.sendTranslated(POSITIVE, "Your book is now unsigned and ready to be edited.");
+        return true;
     }
 
     /**
      * Edits the sign the user is looking at
      *
-     * @param user the user
+     * @param user  the user
      * @param line1 the 1st line
      * @param line2 the 2nd line
      * @param line3 the 3rd line
      * @param line4 the 4th line
-     * @return false if there is no sign
+     *
+     * @return false if there is no sign to edit in sight
      */
     public boolean editSignInSight(User user, String line1, String line2, String line3, String line4)
     {
-        Block target = user.getTargetBlock(Collections.<Material>emptySet(), 10);
-        if (target.getType() == WALL_SIGN || target.getType() == SIGN_POST)
+        Block target = user.getTargetBlock((Set<Material>)null, 10);
+        if (target.getType() != WALL_SIGN && target.getType() != SIGN_POST)
         {
-            Sign sign = (Sign)target.getState();
-            String[] lines = sign.getLines();
-            lines[0] = line1 == null ? lines[0] : line1;
-            lines[1] = line2 == null ? lines[1] : line2;
-            lines[2] = line3 == null ? lines[2] : line3;
-            lines[3] = line4 == null ? lines[3] : line4;
-            SignChangeEvent event = new SignChangeEvent(sign.getBlock(), user, lines);
-            user.getCore().getEventManager().fireEvent(event);
-            if (event.isCancelled())
-            {
-                user.sendTranslated(NEGATIVE, "Could not change the sign!");
-                return true;
-            }
-            for (int i = 0; i < 4; ++i)
-            {
-                sign.setLine(i, lines[i]);
-            }
-            sign.update();
-            user.sendTranslated(POSITIVE, "The sign has been changed!");
+            return false; // No Sign in sight
+        }
+        Sign sign = (Sign)target.getState();
+        String[] lines = sign.getLines();
+        lines[0] = line1 == null ? lines[0] : line1;
+        lines[1] = line2 == null ? lines[1] : line2;
+        lines[2] = line3 == null ? lines[2] : line3;
+        lines[3] = line4 == null ? lines[3] : line4;
+        SignChangeEvent event = new SignChangeEvent(sign.getBlock(), user, lines);
+        user.getCore().getEventManager().fireEvent(event);
+        if (event.isCancelled())
+        {
+            user.sendTranslated(NEGATIVE, "Could not change the sign!");
             return true;
         }
-        // No Sign in sight
-        return false;
-    }
-
-    /**
-     * Unsigns a written book in the hand of given user
-     *
-     * @param user the user
-     * @return false if there is no written book in the hand of given user
-     */
-    public boolean unsignBook(User user)
-    {
-        if (user.getItemInHand().getType() == Material.WRITTEN_BOOK)
+        for (int i = 0; i < 4; ++i)
         {
-            ItemStack item = user.getItemInHand();
-            BookMeta meta = ((BookMeta)item.getItemMeta());
-            meta.setAuthor("");
-            meta.setTitle("");
-            item.setItemMeta(meta);
-            item.setType(Material.BOOK_AND_QUILL);
-            user.sendTranslated(POSITIVE, "Your book is now unsigned and ready to be edited.");
-            return true;
+            sign.setLine(i, lines[i]);
         }
-        return false;
+        sign.update();
+        user.sendTranslated(POSITIVE, "The sign has been changed!");
+        return true;
     }
 }
