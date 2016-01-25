eb5f0044d089d4919c004cc9e3b3749aca3cc111
Anselm Brehme <Faithcaio@cubeisland.de>
2016-01-25 22:07:31 +0100

    writer module

--- writer/src/main/java/org/cubeengine/module/writer/Writer.java
+++ writer/src/main/java/org/cubeengine/module/writer/Writer.java
@@ -17,27 +17,29 @@
  */
 package org.cubeengine.module.writer;
 
+import java.util.Optional;
 import javax.inject.Inject;
-import com.google.common.base.Optional;
+import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
+import de.cubeisland.engine.modularity.core.Module;
+import de.cubeisland.engine.modularity.core.marker.Disable;
+import de.cubeisland.engine.modularity.core.marker.Enable;
 import org.cubeengine.butler.filter.Restricted;
 import org.cubeengine.butler.parametric.Command;
 import org.cubeengine.butler.parametric.Label;
 import org.cubeengine.butler.parametric.Named;
-import de.cubeisland.engine.modularity.core.marker.Disable;
-import de.cubeisland.engine.modularity.core.marker.Enable;
-import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
-import de.cubeisland.engine.modularity.core.Module;
 import org.cubeengine.service.command.CommandManager;
-import org.cubeengine.service.user.User;
-import org.spongepowered.api.Game;
+import org.cubeengine.service.i18n.I18n;
 import org.spongepowered.api.block.BlockType;
-import org.spongepowered.api.data.manipulator.item.PagedData;
-import org.spongepowered.api.data.manipulator.tileentity.SignData;
+import org.spongepowered.api.data.manipulator.mutable.item.PagedData;
+import org.spongepowered.api.data.manipulator.mutable.tileentity.SignData;
+import org.spongepowered.api.data.value.mutable.ListValue;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.inventory.ItemStack;
-import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.util.blockray.BlockRay;
 import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
 import static org.cubeengine.service.i18n.formatter.MessageType.NEGATIVE;
 import static org.cubeengine.service.i18n.formatter.MessageType.POSITIVE;
@@ -45,7 +47,6 @@ import static org.spongepowered.api.block.BlockTypes.STANDING_SIGN;
 import static org.spongepowered.api.block.BlockTypes.WALL_SIGN;
 import static org.spongepowered.api.item.ItemTypes.WRITABLE_BOOK;
 import static org.spongepowered.api.item.ItemTypes.WRITTEN_BOOK;
-import static org.spongepowered.api.util.blockray.BlockRay.ONLY_AIR_FILTER;
 
 /**
  * A module to edit signs and signed books
@@ -54,7 +55,7 @@ import static org.spongepowered.api.util.blockray.BlockRay.ONLY_AIR_FILTER;
 public class Writer extends Module
 {
     @Inject private CommandManager cm;
-    @Inject private Game game;
+    @Inject private I18n i18n;
 
     @Enable
     public void onEnable()
@@ -69,8 +70,8 @@ public class Writer extends Module
     }
 
     @Command(alias = "rewrite", desc = "Edit a sign or a signed book")
-    @Restricted(value = User.class, msg = "Edit what?")
-    public void edit(User context,
+    @Restricted(value = Player.class, msg = "Edit what?")
+    public void edit(Player context,
                      @Named({"1", "Line1"}) @Label("1st line") String line1,
                      @Named({"2", "Line2"}) @Label("2st line") String line2,
                      @Named({"3", "Line3"}) @Label("3st line") String line3,
@@ -80,15 +81,15 @@ public class Writer extends Module
         {
             if (!this.editBookInHand(context))
             {
-                context.sendTranslated(NEGATIVE, "You need to specify at least one parameter to edit a sign!");
-                context.sendTranslated(NEGATIVE, "Or hold a signed book in your hand to edit it.");
+                i18n.sendTranslated(context, NEGATIVE, "You need to specify at least one parameter to edit a sign!");
+                i18n.sendTranslated(context, NEGATIVE, "Or hold a signed book in your hand to edit it.");
             }
             return;
         }
         if (!this.editSignInSight(context, line1, line2, line3, line4))
         {
-            context.sendTranslated(NEGATIVE, "You need to be looking at a sign less than {amount} blocks away!", 10);
-            context.sendTranslated(NEGATIVE, "Or hold a signed book in your hand to edit it.");
+            i18n.sendTranslated(context, NEGATIVE, "You need to be looking at a sign less than {amount} blocks away!", 10);
+            i18n.sendTranslated(context, NEGATIVE, "Or hold a signed book in your hand to edit it.");
         }
     }
 
@@ -99,19 +100,19 @@ public class Writer extends Module
      *
      * @return false if there is no written book in the hand of given user
      */
-    public boolean editBookInHand(User user)
+    public boolean editBookInHand(Player user)
     {
-        Optional<ItemStack> oItem = user.asPlayer().getItemInHand();
+        Optional<ItemStack> oItem = user.getItemInHand();
         if (!oItem.isPresent() || oItem.get().getItem() != WRITTEN_BOOK)
         {
             return false;
         }
 
         ItemStack item = oItem.get();
-        PagedData pages = item.getData(PagedData.class).get();
-        item = game.getRegistry().getItemBuilder().itemType(WRITABLE_BOOK).itemData(pages).build();
-        user.asPlayer().setItemInHand(item);
-        user.sendTranslated(POSITIVE, "Your book is now unsigned and ready to be edited.");
+        PagedData pages = item.get(PagedData.class).get();
+        item = ItemStack.builder().itemType(WRITABLE_BOOK).itemData(pages).build();
+        user.setItemInHand(item);
+        i18n.sendTranslated(user, POSITIVE, "Your book is now unsigned and ready to be edited.");
         return true;
     }
 
@@ -126,27 +127,28 @@ public class Writer extends Module
      *
      * @return false if there is no sign to edit in sight
      */
-    public boolean editSignInSight(User user, String line1, String line2, String line3, String line4)
+    public boolean editSignInSight(Player user, String line1, String line2, String line3, String line4)
     {
-        Optional<BlockRayHit> end = BlockRay.from(user.asPlayer().getLocation().add(0,1.62,0)).filter(ONLY_AIR_FILTER).blockLimit(10).end();
+        Optional<BlockRayHit<World>> end = BlockRay.from(user.getLocation().add(0,1.62,0)).filter(BlockRay.onlyAirFilter()).blockLimit(10).end();
         if (!end.isPresent())
         {
             return false;
         }
-        Location block = end.get().getLocation();
+        Location<World> block = end.get().getLocation();
         BlockType type = block.getBlockType();
         if (type != WALL_SIGN && type != STANDING_SIGN)
         {
             return false;
         }
-        SignData signData = block.getData(SignData.class).get();
-        signData.setLine(0, line1 == null ? signData.getLine(0) : Texts.of(line1));
-        signData.setLine(1, line2 == null ? signData.getLine(1) : Texts.of(line2));
-        signData.setLine(2, line3 == null ? signData.getLine(2) : Texts.of(line3));
-        signData.setLine(3, line4 == null ? signData.getLine(3) : Texts.of(line4));
+        SignData signData = block.get(SignData.class).get();
+        ListValue<Text> lines = signData.lines();
+        lines.set(0, line1 == null ? lines.get(0) : Text.of(line1));
+        lines.set(1, line2 == null ? lines.get(1) : Text.of(line2));
+        lines.set(2, line3 == null ? lines.get(2) : Text.of(line3));
+        lines.set(3, line4 == null ? lines.get(3) : Text.of(line4));
         block.offer(signData);
 
-        user.sendTranslated(POSITIVE, "The sign has been changed!");
+        i18n.sendTranslated(user, POSITIVE, "The sign has been changed!");
         return true;
     }
 }
