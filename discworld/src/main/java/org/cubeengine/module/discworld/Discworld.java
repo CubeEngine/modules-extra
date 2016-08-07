043a7b3557a6a67176deec24e9fc41fda88edd1f
Anselm Brehme <Faithcaio@cubeisland.de>
2016-08-07 12:03:50 +0200

    discworld module

---
+++ discworld/src/main/java/org/cubeengine/module/discworld/Discworld.java
@@ -0,0 +1,9 @@
+package org.cubeengine.module.discworld;
+
+import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
+import de.cubeisland.engine.modularity.core.Module;
+
+@ModuleInfo(name = "Discworld", description = "Provides a custom world generator for a discworld")
+public class Discworld extends Module
+{
+}
