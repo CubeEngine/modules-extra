e8227507366815d13e17788598371a2c1bd57ff1
Anselm Brehme <Faithcaio@cubeisland.de>
2017-06-03 19:52:35 +0200

    doctor! we got a patient!
    formatting

---
+++ patient/src/main/java/org/cubeengine/module/patient/Patient.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of CubeEngine.
+ * CubeEngine is licensed under the GNU General Public License Version 3.
+ *
+ * CubeEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.cubeengine.module.patient;
+
+import de.cubeisland.engine.modularity.asm.marker.ModuleInfo;
+import de.cubeisland.engine.modularity.core.Module;
+import de.cubeisland.engine.modularity.core.marker.Enable;
+import org.cubeengine.libcube.service.filesystem.ModuleConfig;
+
+@ModuleInfo(name = "Patient", description = "Find out what your server is suffering from.")
+public class Patient extends Module
+{
+    @ModuleConfig private PatientConfig config;
+    
+    @Enable
+    public void onEnable()
+    {
+        // TODO analyze stuff running on that server...
+    }
+
+    // TODO command to enable / disable
+    // TODO command to enable SPAM mode (show data every few seconds)
+}
