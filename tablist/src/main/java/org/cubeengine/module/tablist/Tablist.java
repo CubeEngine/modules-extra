ed614a0ce7b8b98c3924a2a38d2ada33074e77c7
Anselm Brehme <Faithcaio@cubeisland.de>
2017-09-24 14:12:10 +0200

    fix?

--- tablist/src/main/java/org/cubeengine/module/tablist/Tablist.java
+++ tablist/src/main/java/org/cubeengine/module/tablist/Tablist.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of CubeEngine.
+ * CubeEngine is licensed under the GNU General Public License Version 3.
+ *
+ * CubeEngine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CubeEngine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CubeEngine.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cubeengine.module.tablist;
 
 import static org.spongepowered.api.text.serializer.TextSerializers.FORMATTING_CODE;
@@ -12,7 +29,6 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.serializer.TextSerializers;
 
 import javax.inject.Singleton;
 
@@ -42,7 +58,7 @@ public class Tablist extends CubeEngineModule
         {
             if (tle.getProfile().equals(player.getProfile()))
             {
-                tle.setDisplayName(Text.of(FORMATTING_CODE.deserialize(prefix + tle.getDisplayName().orElse(Text.EMPTY)).toPlain()));
+                tle.setDisplayName(FORMATTING_CODE.deserialize(prefix + tle.getProfile().getName().orElse("???")));
             }
         }
 
