8c09535b696e2bf0cfa4f6990fb0238edb1fc165
Anselm Brehme <Faithcaio@cubeisland.de>
2018-02-28 17:20:46 +0100

    elevator config

--- elevator/src/main/java/org/cubeengine/module/elevator/ElevatorConfig.java
+++ elevator/src/main/java/org/cubeengine/module/elevator/ElevatorConfig.java
@@ -17,6 +17,7 @@
  */
 package org.cubeengine.module.elevator;
 
+import org.cubeengine.reflect.annotations.Comment;
 import org.cubeengine.reflect.codec.yaml.ReflectedYaml;
 import org.spongepowered.api.item.ItemType;
 import org.spongepowered.api.item.ItemTypes;
@@ -24,10 +25,12 @@ import org.spongepowered.api.item.ItemTypes;
 @SuppressWarnings("all")
 public class ElevatorConfig extends ReflectedYaml
 {
+    @Comment("Dekor String to the left and right of the second line")
     public String liftDecor = "۩";
+    @Comment("Dekor String to show elevators going up")
     public String upDecor = "▲";
+    @Comment("Dekor String to show elevators going down")
     public String downDecor = "▼";
-
+    @Comment("The ItemType to use to create elevator signs")
     public ItemType creationItem = ItemTypes.ENDER_PEARL;
-
 }
