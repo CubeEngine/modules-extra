e6bf5c6e0557bfe4e6b87d8d500cca5ebca959f0
Anselm Brehme <development.anselm@breh.me>
2023-10-02 11:49:26 +0200

    signs are editable in vanilla now

--- writer/src/main/java/org/cubeengine/module/writer/Writer.java
+++ writer/src/main/java/org/cubeengine/module/writer/Writer.java
@@ -17,28 +17,12 @@
  */
 package org.cubeengine.module.writer;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import net.kyori.adventure.text.Component;
-import org.cubeengine.libcube.util.ComponentUtil;
-import org.cubeengine.libcube.util.EventUtil;
 import org.cubeengine.processor.Module;
-import org.spongepowered.api.data.Keys;
-import org.spongepowered.api.data.type.HandTypes;
-import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.block.InteractBlockEvent;
-import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.lifecycle.RegisterDataEvent;
 import org.spongepowered.api.event.lifecycle.RegisterDataPackValueEvent;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.recipe.RecipeRegistration;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.math.vector.Vector3i;
 
 /**
  * A module to edit signs and signed books
@@ -47,8 +31,6 @@ import org.spongepowered.math.vector.Vector3i;
 @Module
 public class Writer
 {
-    @Inject private WriterPermissions perms;
-
     @Listener
     public void onRegisterData(RegisterDataEvent event)
     {
@@ -60,27 +42,4 @@ public class Writer
     {
         WriterItems.registerRecipes(event);
     }
-
-    @Listener
-    public void onRightClickBlock(InteractBlockEvent.Secondary event, @First ServerPlayer player)
-    {
-
-        if (!EventUtil.isMainHand(event.context())
-            || !event.block().location().flatMap(Location::blockEntity).map(l -> l.supports(Keys.SIGN_LINES)).orElse(false)
-            || !player.itemInHand(HandTypes.MAIN_HAND).get(WriterData.WRITER).isPresent()
-            || !player.itemInHand(HandTypes.MAIN_HAND).type().isAnyOf(ItemTypes.WRITABLE_BOOK)
-            || !perms.EDIT_SIGN.check(player))
-        {
-            return;
-        }
-        List<String> pages = player.itemInHand(HandTypes.MAIN_HAND).get(Keys.PLAIN_PAGES).orElse(Collections.emptyList());
-        final String firstPage = pages.get(0);
-        final List<String> lines = Arrays.asList(firstPage.split("\n"));
-        final List<Component> signLines = lines.subList(0, Math.min(4, lines.size())).stream()
-               .map(ComponentUtil::fromLegacy).collect(Collectors.toList());
-        final Vector3i pos = event.block().position();
-        player.world().offer(pos, Keys.SIGN_LINES, signLines);
-
-    }
-
 }
--- writer/src/main/java/org/cubeengine/module/writer/WriterItems.java
+++ writer/src/main/java/org/cubeengine/module/writer/WriterItems.java
@@ -17,23 +17,18 @@
  */
 package org.cubeengine.module.writer;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import net.kyori.adventure.text.serializer.plain.PlainComponentSerializer;
 import org.spongepowered.api.ResourceKey;
 import org.spongepowered.api.data.Keys;
 import org.spongepowered.api.event.lifecycle.RegisterDataPackValueEvent;
 import org.spongepowered.api.item.ItemTypes;
-import org.spongepowered.api.item.enchantment.Enchantment;
-import org.spongepowered.api.item.enchantment.EnchantmentTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.query.QueryTypes;
 import org.spongepowered.api.item.recipe.RecipeRegistration;
 import org.spongepowered.api.item.recipe.crafting.CraftingRecipe;
-import org.spongepowered.api.item.recipe.crafting.Ingredient;
 
 import static org.spongepowered.api.item.ItemTypes.WRITABLE_BOOK;
 import static org.spongepowered.api.item.ItemTypes.WRITTEN_BOOK;
@@ -42,26 +37,6 @@ public class WriterItems
 {
     public static void registerRecipes(RegisterDataPackValueEvent<RecipeRegistration>event)
     {
-        final ItemStack stack = ItemStack.of(WRITABLE_BOOK);
-        stack.offer(Keys.HIDE_ENCHANTMENTS, true);
-        stack.offer(Keys.APPLIED_ENCHANTMENTS, Arrays.asList(Enchantment.of(EnchantmentTypes.MENDING, 1)));
-        stack.offer(WriterData.WRITER, true);
-
-        final RecipeRegistration signWriterBook = CraftingRecipe
-            .shapedBuilder().aisle("sss", "sbs", "sss")
-            .where('s', Ingredient.of(ItemTypes.ACACIA_SIGN, ItemTypes.BIRCH_SIGN, ItemTypes.DARK_OAK_SIGN, ItemTypes.JUNGLE_SIGN, ItemTypes.OAK_SIGN, ItemTypes.SPRUCE_SIGN))
-            .where('b', Ingredient.of(WRITABLE_BOOK))
-            .result(grid -> {
-                final ItemStack newStack = grid.peekAt(4).get();
-                newStack.offer(WriterData.WRITER, true);
-                newStack.offer(Keys.HIDE_ENCHANTMENTS, true);
-                newStack.offer(Keys.APPLIED_ENCHANTMENTS, Arrays.asList(Enchantment.of(EnchantmentTypes.MENDING, 1)));
-                return newStack;
-             }, stack)
-            .key(ResourceKey.of(PluginWriter.WRITER_ID, "signwriterbook"))
-            .build();
-        event.register(signWriterBook);
-
         final RecipeRegistration revertWrittenBook = CraftingRecipe
             .shapelessBuilder().addIngredients(ItemTypes.LEATHER, WRITTEN_BOOK)
             .result(grid -> {
